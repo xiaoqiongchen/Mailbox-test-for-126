455e6d401ad67dfbb538d7fa6b7d959dac1f7ed1
172787078@qq.com <chenxiaoqiong945>
2021-02-27 00:52:04 +0800

    modify some files

--- TestScripts/test.py
+++
@@ -1,8 +0,0 @@
-from Conf.readexcel import ExcelUtil
-from Conf import ConfigPath
-
-file_path=ConfigPath.test_data_path()
-def get_send_email_data():#发送邮件测试数据
-    eu = ExcelUtil(file_path, "send_email")
-    return eu.dict_data()
-print(get_send_email_data())
